5fe4f3770c27f079f89e1a866ffa7085b83849d4
srjiang <srjiang@outlook.com>
2015-07-02 02:12:10 +0800

    add auto-complete function, need to finish

--- frontend/application/entry.js
+++ frontend/application/entry.js
@@ -29,6 +29,40 @@ careerApp
         transclude: true,
         scope: {},
         link: function(scope, element, attrs, formCtrl) {
+            $('#jobKd').autocomplete({
+                // serviceUrl: 'application/test.json',
+                serviceUrl: 'http://120.26.209.92:9998/solr/jobsearch_shard1_replica1/suggest?suggest=true&suggest.build=true&suggest.dictionary=mySuggester&wt=json',
+                paramName: 'suggest.q',
+                dataType:  'jsonp',
+                // lookup: function(query, done) {
+                //     var result = {
+                //         suggestions: [
+                //             { "value": "PHP Junior Programmer", "data": "PHP Junior"},
+                //             { "value": "PHP Senior Programmer", "data": "PHP Senior"},
+                //             { "value": "PHP Team Lead", "data": "PHP Team Lead"}
+                //         ]
+                //     };
+
+                //     done(result);
+                // },
+                transformResult: function(response) {
+                    alert(response);
+                    console.log(response);
+                    return {
+                        // suggestions: $.map(response.myData, function(dataItem) {
+                        //     return { value: dataItem.v, data: dataItem.d };
+                        // })
+                        suggestions: [
+                            { "value": "PHP Junior Programmer", "data": "PHP Junior"},
+                            { "value": "PHP Senior Programmer", "data": "PHP Senior"},
+                            { "value": "PHP Team Lead", "data": "PHP Team Lead"}
+                        ]
+                    };
+                },
+                onSelect: function(suggestion) {
+                    alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+                }
+            });
         },
         templateUrl: 'application/views/jobForm.html'
         // template: '<div class="job-form" ng-transclude></div>'
