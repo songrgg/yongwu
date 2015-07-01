2d075fc068510bc5edd2eac3f8d26da6cf747824
srjiang <srjiang@outlook.com>
2015-07-01 18:23:16 +0800

    add the index page

--- frontend/application/entry.js
+++ frontend/application/entry.js
@@ -1,12 +1,40 @@
 // Career Application.
 "use strict";
 var careerApp = angular.module('careerApp',['ngRoute']);
+
 careerApp.controller('IndexCtrl', ['$scope', '$rootScope',
     function($scope, $rootScope) {
         "use strict";
         console.log("test");
     }
 ]);
+
+// searchForm directive
+careerApp
+.directive('jobSearcher', function() {
+    return {
+        restrict: 'E',
+        transclude: true,
+        scope: {},
+        controller: function($scope) {
+        },
+        // templateUrl: 'application/views/jobSearcher.html'
+        template: '<div class="job-searcher" ng-transclude></div>'
+    };
+})
+.directive('jobForm', function() {
+    return {
+        require: '^jobSearcher',
+        restrict: 'E',
+        transclude: true,
+        scope: {},
+        link: function(scope, element, attrs, formCtrl) {
+        },
+        templateUrl: 'application/views/jobForm.html'
+        // template: '<div class="job-form" ng-transclude></div>'
+    }
+});
+
 angular.module('careerApp').config(['$routeProvider',function ($routeProvider) {
     $routeProvider
         .when('/index', {
@@ -14,6 +42,6 @@ angular.module('careerApp').config(['$routeProvider',function ($routeProvider) {
           controller: 'IndexCtrl'
         })
         .otherwise({
-           redirectTo: '/index'
+          redirectTo: '/index'
         });
 }]);
