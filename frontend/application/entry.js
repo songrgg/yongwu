6944e82c0d8a95ad1ae2e21fff89f0f97415ec90
srjiang <srjiang@outlook.com>
2015-07-02 16:54:05 +0800

    add frontend autocomplete

--- frontend/application/entry.js
+++ frontend/application/entry.js
@@ -5,7 +5,6 @@ var careerApp = angular.module('careerApp',['ngRoute']);
 careerApp.controller('IndexCtrl', ['$scope', '$rootScope',
     function($scope, $rootScope) {
         "use strict";
-        console.log("test");
     }
 ]);
 
@@ -30,42 +29,21 @@ careerApp
         scope: {},
         link: function(scope, element, attrs, formCtrl) {
             $('#jobKd').autocomplete({
-                // serviceUrl: 'application/test.json',
-                serviceUrl: 'http://120.26.209.92:9998/solr/jobsearch_shard1_replica1/suggest?suggest=true&suggest.build=true&suggest.dictionary=mySuggester&wt=json',
-                paramName: 'suggest.q',
+                serviceUrl: 'http://120.26.209.92:8888/jobsolr/suggest/',
+                // paramName: 'suggest.q',
                 dataType:  'jsonp',
-                // lookup: function(query, done) {
-                //     var result = {
-                //         suggestions: [
-                //             { "value": "PHP Junior Programmer", "data": "PHP Junior"},
-                //             { "value": "PHP Senior Programmer", "data": "PHP Senior"},
-                //             { "value": "PHP Team Lead", "data": "PHP Team Lead"}
-                //         ]
-                //     };
-
-                //     done(result);
-                // },
-                transformResult: function(response) {
-                    alert(response);
-                    console.log(response);
-                    return {
-                        // suggestions: $.map(response.myData, function(dataItem) {
-                        //     return { value: dataItem.v, data: dataItem.d };
-                        // })
-                        suggestions: [
-                            { "value": "PHP Junior Programmer", "data": "PHP Junior"},
-                            { "value": "PHP Senior Programmer", "data": "PHP Senior"},
-                            { "value": "PHP Team Lead", "data": "PHP Team Lead"}
-                        ]
-                    };
+                ajaxSettings: {
+                    jsonpCallback: 'jsonCallback',
+                    success: function(data) {
+                        console.log('return ok');
+                    },
                 },
                 onSelect: function(suggestion) {
-                    alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+                    console.log('You selected: ' + suggestion.value + ', ' + suggestion.data);
                 }
             });
         },
         templateUrl: 'application/views/jobForm.html'
-        // template: '<div class="job-form" ng-transclude></div>'
     }
 });
 
